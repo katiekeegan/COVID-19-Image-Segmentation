15e8d057ba7bede0b802118cf5fe71a39d196b98
katiekeegan <66965763+katiekeegan@users.noreply.github.com>
2021-10-28 17:25:15 -0400

    Update rf_training.py

--- code/rf_training.py
+++ code/rf_training.py
@@ -50,7 +50,7 @@ for i in range(0,30):
     errors_binary.append(tools.average_dice(y_preds_binary,y_val))
     print(i, errors_binary[i])
     
-    if i > 3 and errors_binary[i] > errors_binary[i-1] > errors_binary[i-2]:
+    if i > 2 and errors_binary[i] > errors_binary[i-1] > errors_binary[i-2]:
         joblib.dump(rf_model, '../results/rf_model_{}.joblib'.format(rf_model.n_estimators))
 
 #create dataframe from data
